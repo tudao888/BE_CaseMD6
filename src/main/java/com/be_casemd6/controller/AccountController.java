da73e939298f0a3cb149a890e5152bd67701b578
conPuppy <hoangnhatnam29102000@gmail.com>
2023-03-10 11:24:21 +0700

    edit package 2

--- src/main/java/com/be_casemd6/controller/AccountController.java
+++ src/main/java/com/be_casemd6/controller/AccountController.java
@@ -14,34 +14,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/register")
 public class AccountController {
     @Autowired
-    private IAccountService accountService;
+    private IAccountService iAccountService;
     @Autowired
-    private IProviderService providerService;
+    private IProviderService iProviderService;
     @Autowired
     private IEmailService emailService;
+
     @PostMapping
-    public ResponseEntity<Account> createAccountAndProviderDefault(@RequestBody Account account){
+    public ResponseEntity<Account> createAccountAndProviderDefault(@RequestBody Account account) {
         account.setDateOfRegister(new Date().toString());
         account.setStatusAccount(0);
         account.setWallet(0.0);
         account.setStatusComment(0);
-        accountService.createAccount(account);
-        Account account1=accountService.findAccountByUsername(account.getUsername());
-        Provider provider= new Provider();
+        iAccountService.createAccount(account);
+        Account account1 = iAccountService.findAccountByUsername(account.getUsername());
+        Provider provider = new Provider();
         provider.setAccount(account1);
         provider.setStatusProvider(0);
         provider.setStatusVIP(0);
-        providerService.createProvider(provider);
-        EmailDetails emailDetails=new EmailDetails(account1.getEmail());
-        emailService.sendSimpleMail(emailDetails,account1.getUsername(),account1.getPassword());
+        iProviderService.createProvider(provider);
+        EmailDetails emailDetails = new EmailDetails(account1.getEmail());
+        emailService.sendSimpleMail(emailDetails, account1.getUsername(), account1.getPassword());
         return new ResponseEntity<>(account1, HttpStatus.OK);
-        @GetMapping
+    }
+
+    @GetMapping
     public ResponseEntity<List<Account>> getAllProvider() {
         return new ResponseEntity<>(iAccountService.getAllProvider(), HttpStatus.OK);
     }
