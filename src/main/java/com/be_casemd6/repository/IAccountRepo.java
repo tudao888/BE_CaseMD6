7c12109692d059f9401e5cd0d46afa3ffc42fb81
conPuppy <hoangnhatnam29102000@gmail.com>
2023-03-10 10:58:45 +0700

    edit package

--- src/main/java/com/be_casemd6/repository/IAccountRepo.java
+++
@@ -1,8 +0,0 @@
-package com.be_casemd6.repository;
-
-import com.be_casemd6.model.Account;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface IAccountRepo extends PagingAndSortingRepository<Account,Integer> {
-    Account findAccountByUsername(String username);
-}
--- src/main/java/com/be_casemd6/service/IAccountService.java
+++ src/main/java/com/be_casemd6/service/IAccountService.java
@@ -2,6 +2,8 @@ package com.be_casemd6.service;
 
 import com.be_casemd6.model.Account;
 
+import java.util.List;
+
 public interface IAccountService {
     Account createAccount (Account account);
     Account findAccountByUsername(String username);
