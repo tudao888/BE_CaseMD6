39fd2373241a7a1d81f0e52f69ef09165f70214d
conPuppy <hoangnhatnam29102000@gmail.com>
2023-03-10 09:49:14 +0700

    1

--- src/main/java/com/be_casemd6/service/IProviderService.java
+++ src/main/java/com/be_casemd6/service/IProviderService.java
@@ -4,6 +4,8 @@ import com.be_casemd6.model.Provider;
 
 import org.aspectj.apache.bcel.classfile.Module;
 
+import java.util.List;
+
 public interface IProviderService {
     Provider createProvider (Provider provider);
     List<Provider> getAllProviderAcc();
